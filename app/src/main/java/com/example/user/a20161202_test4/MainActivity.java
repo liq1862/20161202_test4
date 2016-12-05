af384d3c41b71f7810660a0f1277b6c2eaf98ea7
lili <liq1862@gmail.com>
2016-12-05 09:10:39 +0800

    加入ListView顯示

--- app/src/main/java/com/example/user/a20161202_test4/MainActivity.java
+++ app/src/main/java/com/example/user/a20161202_test4/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -11,18 +12,20 @@ import com.example.user.a20161202_test4.data.Student;
 import com.example.user.a20161202_test4.data.StudentDAO;
 import com.example.user.a20161202_test4.data.StudentDAOMemoryImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> data;
     ListView lv;
-
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         lv = (ListView) findViewById(R.id.listView);
-
+        data = new ArrayList<>();
 
         StudentDAO dao = new StudentDAOMemoryImpl();
         dao.add(new Student(1, "Bob", "123", "aa123"));
@@ -33,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d("DATA", s.toString());
         }
 
-
-        
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
     }
 }
